cb08a63ef46d7e7add48ea926242fb6fec7d66c3
TheLazyHatGuy <1010359+TheLazyHatGuy@users.noreply.github.com>
2021-05-03 01:47:21 +0100

    Fix metal detector collision

--- Source/Sound_Implementation/Sound_ImplementationCharacter.cpp
+++ Source/Sound_Implementation/Sound_ImplementationCharacter.cpp
@@ -21,7 +21,7 @@ DEFINE_LOG_CATEGORY_STATIC(LogFPChar, Warning, All);
 ASound_ImplementationCharacter::ASound_ImplementationCharacter()
 {
 	// Set size for collision capsule
-	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
+	GetCapsuleComponent()->InitCapsuleSize(30.f, 96.0f);
 
 	// set our turn rates for input
 	BaseTurnRate = 45.f;
